c27446f5bec4e8379b684aa6beba9c9c07d2fecc
unknown <david1990m@gmail.com>
2020-11-11 13:02:06 -0700

    Added notation

--- ExceptionsExample/src/com/exceptions/EvenNumberException.java
+++ ExceptionsExample/src/com/exceptions/EvenNumberException.java
@@ -6,7 +6,7 @@ public class EvenNumberException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 5978561171858943060L;
-
+	//ovveriding getMessageMethod from Exception class
 	@Override
 	public String getMessage() {
 		return "You cannot input an even number.";
--- ExceptionsExample/src/com/exceptions/Simulation.java
+++ ExceptionsExample/src/com/exceptions/Simulation.java
@@ -5,34 +5,36 @@ import java.util.Scanner;
 public class Simulation {
 
 	public static void main(String[] args) {
+		//runs method get input and makes it catch custom exception
 		try {
 			getInput();
 		} catch (EvenNumberException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+	//main logic
 	public static void getInput() throws EvenNumberException{
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Enter a number");
 		String line = input.nextLine();
 		System.out.println("You typed " + line);
-		
+		//this passes user input through even number checker
 		if (isEven(line)) {
 			try {
+				//tells the code that if it is even throw this custom error
 			throw new EvenNumberException();
 			} finally {
+				//closing scanner
 				input.close();
 			}
 		}
-		
+		//closing scanner
 		input.close();
 		
 		
 	}
-	
+	//method that finds even numbers
 	public static boolean isEven(String num) {
 		int value = Integer.parseInt(num);
 		
